63b059c90d9e7a5b947ff6c2ccc752e35e11a9d5
sanghyeon <uphyeon@kakao.com>
2023-12-22 21:30:01 +0900

    Add: 프로젝트 초기 셋팅 (#1)

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -2,11 +2,16 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
+    <Html lang="ko">
+      <Head>
+        <title>Keyboduck</title>
+      </Head>
       <body>
+        {/* Header */}
+        {/* Main */}
         <Main />
         <NextScript />
+        {/*  Footer*/}
       </body>
     </Html>
   )
