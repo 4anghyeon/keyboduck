7218c1f18b2e47f8104e6472bb3dffbd4cf0217a
Yeomhyewon <duagpdnjs1@naver.com>
2023-12-27 23:33:54 +0900

    Cleanup: console.log 제거 (#38)

--- src/components/questionModal/Modal.tsx
+++ src/components/questionModal/Modal.tsx
@@ -3,7 +3,6 @@ import styles from '@/components/questionModal/modal.module.css';
 import {ModalDefaultType} from './types/modal';
 
 export const Modal = ({onClickToggleHandler, children}: PropsWithChildren<ModalDefaultType>) => {
-  console.log(children);
   const onRequestClose = (e: React.MouseEvent) => {
     e.preventDefault();
     if (onClickToggleHandler) {
