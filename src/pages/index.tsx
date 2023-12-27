2af3dd7dd23164c61813021f89e749d33208e14f
sanghyeon <uphyeon@kakao.com>
2023-12-27 15:39:48 +0900

    Feat: 카드 슬라이드 추가 (#19)

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,15 +5,17 @@ import {Tables} from '@/shared/supabase/types/supabase';
 
 export default function HomePage({keyboard}: Readonly<{keyboard: Tables<'keyboard'>[]}>) {
   // 출시일 순으로 정렬한 키보드 데이터
-  const recentlyList = keyboard.toSorted((a, b) => {
-    return new Date(b.release_date).getDate() - new Date(a.release_date).getDate();
-  });
+  const recentlyList = keyboard
+    .toSorted((a, b) => {
+      return new Date(b.release_date).getDate() - new Date(a.release_date).getDate();
+    })
+    .slice(0, 5);
 
   console.log(recentlyList);
 
   return (
     <>
-      <RowContainer title={'인기 키보드'} keyboard={keyboard} />
+      <RowContainer title={'인기 키보드'} keyboard={keyboard.slice(0, 10)} />
       <RowContainer title={'새로나온 키보드'} keyboard={recentlyList} />
       <GridContainer keyboard={keyboard} />
     </>
