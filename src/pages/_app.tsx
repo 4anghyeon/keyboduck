83568ca0e3f0ef7f6158a5158ef638b09b010412
yeahzzl <yeahzzl1018@gmail.com>
2023-12-27 12:37:11 +0900

    Design: header,footer UI(#9)

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,14 @@
+import Footer from '@/components/layout/footer/Footer';
+import NavBar from '@/components/layout/navbar/NavBar';
 import '@/styles/globals.css';
 import type {AppProps} from 'next/app';
 
 export default function App({Component, pageProps}: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <div>
+      <NavBar />
+      <Component {...pageProps} />
+      <Footer />
+    </div>
+  );
 }
