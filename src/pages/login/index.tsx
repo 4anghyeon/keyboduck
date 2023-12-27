c1a0f582f557efc27eadd408640cdf781be34671
HIITMEMARIO <alswnsgur@nate.com>
2023-12-27 23:25:41 +0900

    수정중

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,22 +1,50 @@
-import React from 'react';
+import React, {useState} from 'react';
 import login from './index.module.css';
 import {signInWithGithub, googleLogin} from '../api/auth';
 
 const LoginPage = () => {
+  const [idValue, setIdValue] = useState<string>('');
+  const [pwValue, setPwValue] = useState<string>('');
+  console.log(pwValue);
+  console.log(idValue);
+
+  const clickLoginHandler = e => {
+    e.preventDefault();
+  };
+
   return (
     <div className={login.wrapper}>
-      <div className={login.inputbox}>
+      <form className={login.inputbox} onSubmit={clickLoginHandler}>
         <div className={login.input}>
           <p className={login.ptagposition}>이메일</p>
-          <input className={login.inputsize} placeholder="이메일 형식으로 입력해주세요"></input>
+          <input
+            className={login.inputsize}
+            placeholder="이메일 형식으로 입력해주세요"
+            value={idValue}
+            onChange={e => {
+              setIdValue(e.target.value);
+            }}
+            minLength={4}
+          ></input>
         </div>
         <div className={login.input}>
           <p className={login.ptagposition}>비밀번호</p>
-          <input className={login.inputsize} placeholder="최소 4자 이상 입력해주세요"></input>
+          <input
+            className={login.inputsize}
+            type="password"
+            placeholder="최소 4자 이상 입력해주세요"
+            value={pwValue}
+            onChange={e => {
+              setPwValue(e.target.value);
+            }}
+            minLength={4}
+          ></input>
         </div>
-      </div>
+      </form>
       <div className={login.buttonbox}>
-        <button className={login.button}>로그인</button>
+        <button type="submit" className={login.button} disabled={idValue.length < 4 || pwValue.length < 4}>
+          로그인
+        </button>
         <button className={login.button} onClick={googleLogin}>
           Google로 로그인하기
         </button>
