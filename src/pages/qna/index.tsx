de48b1a98521751238e87bd1b2bb4d6378a01eb9
Yeomhyewon <duagpdnjs1@naver.com>
2023-12-27 12:03:21 +0900

    Design: QnA 페이지 UI 구성

--- src/pages/qna/index.tsx
+++ src/pages/qna/index.tsx
@@ -1,7 +1,34 @@
 import React from 'react';
+import styles from '@/pages/qna/index.module.css';
 
 const Qna = () => {
-  return <div>Qna</div>;
+  return (
+    <div className={styles['qna-container']}>
+      <div className={styles['qna-title']}>
+        <h2>QnA</h2>
+      </div>
+      <div className={styles['qna-sreach-bar']}>
+        <input placeholder="검색어를 입력해주세요" />
+        <button>⌕</button>
+      </div>
+      <div className={styles['qna-list']}>
+        <div className={styles['qna-list-title']}>
+          <p>날짜</p>
+          <p>제목</p>
+          <p>작성자</p>
+        </div>
+        <div className={styles['qna-list-item']}>
+          {/* 데이터 들어갈 자리 */}
+          <p>2023-12-25</p>
+          <p>키크론 k8 pro 좋은가요?</p>
+          <p>HelloWorld</p>
+        </div>
+      </div>
+      <div className={styles['qna-registration-btn']}>
+        <button>작성하기</button>
+      </div>
+    </div>
+  );
 };
 
 export default Qna;
