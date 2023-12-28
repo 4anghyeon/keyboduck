6fd81b7c2e3f49140da6413d2a4495e5e8736bc1
sanghyeon <uphyeon@kakao.com>
2023-12-28 11:19:48 +0900

    Feat: 상세페이지 관련 영상 기능 추가 (#31)

--- src/components/keyboard/RelatedVideos.tsx
+++ src/components/keyboard/RelatedVideos.tsx
@@ -1,16 +1,13 @@
 import React from 'react';
 import styles from './related-videos.module.css';
 import YoutubeCard from '@/components/keyboard/YoutubeCard';
+import {Tables} from '@/shared/supabase/types/supabase';
 
-const RelatedVideos = () => {
+const RelatedVideos = ({item}: {item: Tables<'keyboard'>}) => {
   return (
     <section className={styles.container}>
       <h1>관련 영상</h1>
-      <div className={styles.row}>
-        <YoutubeCard />
-        <YoutubeCard />
-        <YoutubeCard />
-      </div>
+      <div className={styles.row}>{item.youtube_link?.map(link => <YoutubeCard key={link} link={link} />)}</div>
     </section>
   );
 };
--- src/components/keyboard/YoutubeCard.tsx
+++ src/components/keyboard/YoutubeCard.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import styles from './youtube-card.module.css';
 
-const YoutubeCard = () => {
+const YoutubeCard = ({link}: {link: string}) => {
+  console.log(link);
   return (
     <div className={styles.container}>
       <iframe
-        src="https://www.youtube.com/embed/zd8wnvT--yU?si=nA1Vu3JfhHVDPKUo&amp;controls=0"
+        src={link}
         title="YouTube video player"
         frameBorder="0"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
