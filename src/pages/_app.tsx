c4c4683f869c4832aa280e5ffa0e04d31ae9a24b
sanghyeon <uphyeon@kakao.com>
2023-12-28 12:21:13 +0900

    Feat: 좋아요 로그인한 유저만 가능 하도록 추가 (#31)

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,6 +4,9 @@ import '@/styles/globals.css';
 import type {AppProps} from 'next/app';
 import {QueryClient} from '@tanstack/query-core';
 import {QueryClientProvider} from '@tanstack/react-query';
+import React from 'react';
+import {ToastContainer} from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 export const queryClient = new QueryClient();
 
@@ -12,6 +15,7 @@ export default function App({Component, pageProps}: AppProps) {
     <QueryClientProvider client={queryClient}>
       <NavBar />
       <Component {...pageProps} />
+      <ToastContainer />
       <Footer />
     </QueryClientProvider>
   );
