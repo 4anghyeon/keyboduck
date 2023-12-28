12f82f562b374f169c5f03357d0c4f58b9127522
Yeomhyewon <duagpdnjs1@naver.com>
2023-12-28 17:49:51 +0900

    Feat: Link tag 추가 및 css 수정 (#43)

--- src/components/question/QuestionList.tsx
+++ src/components/question/QuestionList.tsx
@@ -1,17 +1,19 @@
 import React from 'react';
 import styles from '@/components/question/QuestionList.module.css';
 import {QuestionType} from '@/pages/question/types/question';
+import Link from 'next/link';
 
 const QuestionList = ({question}: {question: QuestionType}) => {
-  console.log(question);
   return (
-    <>
-      <div className={styles['qna-list-item']}>
-        <p>{question.write_date?.substring(0, 10)}</p>
-        <p>{question.title}</p>
-        <p>{question.author}</p>
-      </div>
-    </>
+    <div className={styles['qna-a-tag']}>
+      <Link href={`/question/${question.id}`}>
+        <div className={styles['qna-list-item']}>
+          <p>{question.write_date?.substring(0, 10)}</p>
+          <p>{question.title}</p>
+          <p>{question.author}</p>
+        </div>
+      </Link>
+    </div>
   );
 };
 
