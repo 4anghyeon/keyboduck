448249fb103b0836d1dc4124059f070655d5ff27
Yeomhyewon <duagpdnjs1@naver.com>
2023-12-29 23:57:35 +0900

    Cleanup: use client 없애기 (#82)

--- src/components/question/QuestionDetailComment.tsx
+++ src/components/question/QuestionDetailComment.tsx
@@ -1,5 +1,3 @@
-'use client';
-
 import React, {useEffect, useState} from 'react';
 import styles from '@/components/question/QuestionDetailComment.module.css';
 import {AnswerType} from '@/pages/question/types/question';
